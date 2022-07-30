096ee1379877f11109bcd91f7f797111d2818e20
Marat Kotik <mkotik97@gmail.com>
2022-07-29 23:01:35 -0400

    simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ padding: "0px 200px" }}>
       <ChatWindow />
     </div>
   );
--- src/ChatWindow.tsx
+++ src/ChatWindow.tsx
@@ -10,18 +10,53 @@ const ChatWindow = () => {
     setCurrentMessage("");
   };
   return (
-    <div>
-      <div>
+    <div style={{ backgroundColor: "grey" }}>
+      <div
+        style={{
+          margin: "auto",
+          width: "70%",
+          height: "60vh",
+          textAlign: "left",
+          padding: "20px 0px",
+        }}
+      >
         {messages.map((message) => {
           return <p>{message}</p>;
         })}
       </div>
-      <div style={{ display: "flex" }}>
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "transparent",
+          paddingBottom: "20px",
+          width: "70%",
+          margin: "auto",
+        }}
+      >
         <input
           onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? handleSubmit() : null)}
           value={currentMessage}
+          style={{
+            width: "100%",
+            height: "30px",
+            marginRight: "10px",
+            marginTop: "10px",
+            padding: "5px 20px",
+          }}
         />
-        <button onClick={handleSubmit}>Submit</button>
+        <button
+          onClick={handleSubmit}
+          style={{
+            height: "34px",
+            width: "100px",
+            marginTop: "10px",
+            marginBottom: "0px",
+            padding: "0px",
+          }}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
