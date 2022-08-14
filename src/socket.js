3d2a1784aae201056b24d08b0a30ec9ac2629cf4
Marat Kotik <mkotik97@gmail.com>
2022-08-14 18:53:07 -0400

    create socket.js

---
+++ src/socket.js
@@ -0,0 +1,30 @@
+const io = require("socket.io-client");
+
+const socket = () => {
+  const port =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000"
+      : "http://chatroombe-env.eba-mtuquxgv.us-east-2.elasticbeanstalk.com/";
+  const socket = io(port, {
+    extraHeaders: {
+      "Access-Control-Allow-Credentials": "true",
+    },
+  });
+
+  socket.on("error", (err) => console.log("socket error:", err));
+
+  const sendMessage = (messageObject) => {
+    if (socket) socket.emit("message", messageObject);
+  };
+
+  const sendTypingNotice = () => {
+    if (socket) socket.emit("typing");
+  };
+
+  return {
+    sendMessage,
+    sendTypingNotice,
+  };
+};
+
+export default socket;
