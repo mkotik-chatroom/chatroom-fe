26856fc4b62343b53cbd86e28b79eb72ec66c084
Marat Kotik <mkotik97@gmail.com>
2023-06-01 22:29:13 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [name, setName] = useState<string>("");
-
+  // test
   useEffect(() => {
     if (!socket) {
       setSocket(
